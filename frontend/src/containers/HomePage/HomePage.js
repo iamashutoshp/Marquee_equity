958b3d4ab01959fb9d6a58e3d1e30d90bfefa97a
iamashutoshp <ap1853177@gmail.com>
2022-09-03 02:19:50 +0530

    frontend : landing page Datatable added

--- frontend/src/containers/HomePage/HomePage.js
+++ frontend/src/containers/HomePage/HomePage.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Box, Button, TextField } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import './homepage.css'
 
 export default function HomePage() {
+  const table_cols = [{ field: 'cin', headerName: 'CIN', flex: 2 },
+  { field: 'company', headerName: 'Name', flex: 2 }]
+
+  const table_rows = [{ id: 1, company: 'Amazon', cin: '23' },
+  { id: 2, company: 'Google', cin: '24' },
+  { id: 3, company: 'Microsoft', cin: '25' }]
   return (
     <>
-      <Box sx={{ width: '80%',margin:'auto' }} className='homepage__container'>
+      <Box className='homepage__container'>
         <div className='homepage__container-toprow'>
           <div className='homepage__container-company'>
             <Button variant="contained" color="secondary" size="small">{`COMPANY +`}</Button>
@@ -18,6 +25,24 @@ export default function HomePage() {
 
         <div className='homepage__container-showEntries'>
           <span>show entries</span>
+        </div>
+
+        <div className="homepage__container-datatable-div" style={{ height: '40vh' }}>
+          <DataGrid
+            rows={table_rows}
+            columns={table_cols}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            hideFooter
+            disableSelectionOnClick
+            disableColumnMenu
+            disableColumnSelector
+          />
+          <div className="homepage__container-datatable-div__footer">
+            <Button >PREVIOUS</Button>
+            <Button variant="contained" size="small">{`CUR`}</Button>
+            <Button>NEXT</Button>
+          </div>
         </div>
       </Box>
     </>
